bf6dd1ff348e494a1bf934bc5f5d659ec56de3c0
Jessiley <xjaco222x@hotmail.com>
2020-04-29 22:43:06 -0300

    Feat: Added Log model and Log migration

---
+++ src/app/models/log.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Question extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        user: DataTypes.STRING,
+        where: DataTypes.STRING,
+        type: DataTypes.STRING(10),
+        return: DataTypes.TEXT,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+}
+
+module.exports = Question;
