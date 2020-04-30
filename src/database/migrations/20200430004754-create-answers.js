18b07ae70d3dab38aeb88fa6c1c954b4dd2b3f6c
Jessiley <xjaco222x@hotmail.com>
2020-04-29 21:57:32 -0300

    Fix: Added question_id to the answers migration

--- src/database/migrations/20200430004754-create-answers.js
+++ src/database/migrations/20200430004754-create-answers.js
@@ -17,6 +17,13 @@ module.exports = {
         type: Sequelize.TEXT,
         allowNull: false,
       },
+      question_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'questions', key: 'id' },
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
