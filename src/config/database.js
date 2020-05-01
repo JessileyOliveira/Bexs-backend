d86d53f15d11d21447228760cbf9c26819060c53
Jessiley <xjaco222x@hotmail.com>
2020-05-01 11:45:08 -0300

    Refactor: Added DB_DATABASE in .env file

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ module.exports = {
   host: process.env.DB_HOST || '192.168.99.100',
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'docker',
-  database: 'bexs',
+  database: process.env.DB_DATABASE || 'bexs',
   define: {
     timestamps: true,
     underscored: true,
