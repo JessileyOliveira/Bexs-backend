5c5958065db17272e15086837e2d32b984d54340
Jessiley <xjaco222x@hotmail.com>
2020-04-30 23:44:50 -0300

    Feat: Added answer store route

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,18 @@ const { Router } = require('express');
 const routes = new Router();
 
 const QuestionController = require('./app/controllers/QuestionController');
+const AnswerController = require('./app/controllers/AnswerController');
 
 const getQuestion = require('./app/middlewares/getQuestion');
 
 routes.post('/questions', QuestionController.store);
 routes.get('/questions', QuestionController.index);
 routes.get('/questions/:question_id', getQuestion, QuestionController.show);
+
+routes.post(
+  '/questions/:question_id/answers',
+  getQuestion,
+  AnswerController.store
+);
 
 module.exports = routes;
